7e141d5f357b0c35a6d34f014a8a1d82ffd4f076
agus24 <agusx244@gmail.com>
2021-02-20 14:23:34 +0700

    fix minor bug

--- src/Gustiawan/FormBuilder/Form.php
+++ src/Gustiawan/FormBuilder/Form.php
@@ -54,7 +54,7 @@ class Form
     {
         $this->action = $options['action'];
         $this->method = $options['method'];
-        $this->data = array_key_exists("data", $options) ? $options['data'] : null;
+        $this->data = array_key_exists("data", $options) ? $options['data'] : [];
 
         return $this;
     }
