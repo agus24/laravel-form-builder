27d59142b94d1aa2e2684867dea9bfec54db2815
agus24 <agusx244@gmail.com>
2021-06-28 13:30:34 +0700

    update compatilibility for number input

--- src/Gustiawan/FormBuilder/views/components/bootstrap/fields/input.blade.php
+++ src/Gustiawan/FormBuilder/views/components/bootstrap/fields/input.blade.php
@@ -5,4 +5,8 @@
     value="{{ old($field['name']) ?? $field['value'] }}"
     {{ $field['readonly'] ? "readonly" : "" }}
     {{ $field['disabled'] ? "disabled" : "" }}
-    {{ $field['required'] ? "required" : "" }}>
+    {{ $field['required'] ? "required" : "" }}
+    @if ($field['type'] == 'number')
+        step="any"
+    @endif
+    >
