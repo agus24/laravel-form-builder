33ccb3bb526dcabddc07f096bc5397fb5b4dccb1
gustiawan <agusx244@gmail.com>
2021-09-02 22:12:31 +0700

    update

--- src/Gustiawan/FormBuilder/views/components/base.blade.php
+++ src/Gustiawan/FormBuilder/views/components/base.blade.php
@@ -21,7 +21,7 @@
     function loadDataFor(element, field, url) {
         url = url.replaceAll(`{${field}}`, element.value)
         field = document.querySelector(`#${field}`)
-        fetch(url).then(response => fetchField(field, response)).catch(error => alert(error))
+        fetch(url).then(response => fetchField(field, response.json())).catch(error => alert(error))
     }
 
     function fetchField(field, values) {
