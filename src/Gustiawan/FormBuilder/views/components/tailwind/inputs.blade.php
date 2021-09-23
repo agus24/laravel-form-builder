6b07a5dba24325f112b392d18f36a22675910023
Anis Rahmatul Aziz <aziez1996@gmail.com>
2021-09-23 11:19:15 +0700

    fix error (#11)
    * add custom input field
    
    * update

--- src/Gustiawan/FormBuilder/views/components/tailwind/inputs.blade.php
+++ src/Gustiawan/FormBuilder/views/components/tailwind/inputs.blade.php
@@ -12,7 +12,7 @@
 
         <div class="mb-4">
             <label class="ml-1">{!! $field['label'] !!}</label>
-            @if( !in_array($field['type'], ["text", "date", "password", "number", "file"]))
+            @if( !in_array($field['type'], ["text", "date", "password", "number", "file", "time", "datetime"]))
                 @include('form-generator::components.tailwind.fields.'.$field['type'], ["field" => $field])
             @else
                 @include('form-generator::components.tailwind.fields.input', ["field" => $field])
